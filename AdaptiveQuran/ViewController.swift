33d41a4fed2175cde9c9ca80914edbd12441c2d2
amirshakoor <i150272@nu.edu.pk>
2019-04-08 11:10:36 +0500

    Initial Commit

---
+++ AdaptiveQuran/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdaptiveQuran
+//
+//  Created by Amir Mughal on 08/04/2019.
+//  Copyright © 2019 Amir Mughal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
